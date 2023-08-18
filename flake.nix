1e584464ddea95557ebe60153527f314bbea44c9
Ismail Bello <ismail.bello.513@gmail.com>
2023-08-18 22:29:52 +0100

    use community repo

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    nixvim.url = "github:pta2002/nixvim/nixos-23.05";
+    nixvim.url = "github:nix-community/nixvim/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
