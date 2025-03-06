854cc87f3ec6d0da8ed7e8b3ab56f284cbf20536
Ismail Bello <ismail.bello.513@gmail.com>
2025-03-06 22:51:08 +0000

    chore: sync

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,8 +34,15 @@
 vim.g.mapleader = ';'
 vim.g.maplocalleader = ' '
 
--- enable editorconfig support
+vim.schedule(function()
+  vim.opt.tabstop = 4
+  vim.opt.shiftwidth = 4
+  vim.opt.expandtab = true
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
 vim.g.editorconfig = true
+
 
 -- autochange directories to the openned files
 vim.opt.autochdir = true
@@ -63,10 +70,6 @@ vim.opt.showmode = false
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
 -- Enable break indent
 vim.opt.breakindent = true
 
